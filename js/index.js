8344513488f33f94cb68d15f631a8db00e720d14
Crysnic <vovarx@gmail.com>
2015-03-04 21:48:25 +0200

    Use html buttons to enter data

--- js/index.js
+++ js/index.js
@@ -1,28 +1,65 @@
-function Calculator() {
-  "use strict";
-  var expression = document.getElementById('expr').value;
-  var errElem = document.getElementsByTagName("div")[0];
-
-  this.calculate = function () {
-    try {
-      expression = parseStrExpr(expression);
-      var result = (new Function("return " + expression))();
-      if (!isNumeric(result)) {
-        throw {message: "Невозможная операция", name: "Error"};
-      }
-      document.getElementById('expr').value = result;
-      errElem.children[0].innerHTML = "";
-
-    } catch (e) {
-      errElem.children[0].innerHTML = e.name + ": " + e.message;
-    }
-  };
+// Отправить символ на дисплей
+function sendValue(val) {
+  var display = document.getElementById("display");
+  // отформатировать вывод для знаков(+,-,*,/)
+  display.value +=
+    isNumeric(val) || /(\.|%)/.test(val) ? val : " " + val + " ";
 }
 
-function parseStrExpr(str) {
+// Вычисление выражения с дисплея
+function calculate() {
+  var expression = document.getElementById('display').value;
+  var errElem = document.getElementById("errorMessage");
+  
+  try {
+    expression = parseExpression(expression);
+    var result = (new Function("return " + expression))();
+    
+    // сформировать ошибку (например деление на ноль)
+    if (!isNumeric(result)) {
+      throw {message: "Невозможная операция", name: "Error"};
+    }
+    
+    document.getElementById('display').value = result;
+    errElem.children[0].innerHTML = "";
+    
+  } catch (e) {
+    // любая другая ошибка
+    if (e.name !== "Error") {
+      var errMessage = e.name + ": Некорректное выражение";
+    } else {
+      errMessage = e.name + ": " + e.message;
+    }
+    
+    errElem.children[0].innerHTML = errMessage;
+  }
+}
+
+// Удаление одного символа с дисплея
+function deleteCharacter() {
+  var display = document.getElementById("display");
+  
+  var expressionArray = display.value.split('');
+  // если рядом с символом есть пробел то удалить и его
+  if (expressionArray.pop() === ' ') {
+    expressionArray.pop();
+  }
+  
+  display.value = expressionArray.join('');
+  document.getElementById("errorMessage").children[0].innerHTML = "";
+}
+
+// Очистка дисплея
+function cleanDisplay() {
+  document.getElementById("display").value = "";
+  document.getElementById("errorMessage").children[0].innerHTML = "";
+}
+
+// Internal Functions------------------------------------------------
+function parseExpression(str) {
   "use strict";
-  var regstr = /(cos|sin|tan|acos|asin|atan|log|pow|sqrt)/ig;
-  return str.replace(regstr, "Math.$1");
+  var regstr = /(%)/ig;
+  return str.replace(regstr, "/ 100 ");
 }
 
 function isNumeric(n) {
